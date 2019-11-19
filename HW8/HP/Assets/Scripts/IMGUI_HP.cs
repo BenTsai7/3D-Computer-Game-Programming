15026c43479f4f501a93ef014c7e1859883838f3
BenTsai7 <>
2019-11-19 11:57:11 +0800

    update

---
+++ HW8/HP/Assets/Scripts/IMGUI_HP.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IMGUI_HP : MonoBehaviour
+{
+	private float HP = 0.0f;
+	private float newHP = 0.0f;
+    void OnGUI()
+    {
+
+		if (GUI.Button(new Rect(30, 60, 100, 50), "Add", new GUIStyle("button") { fontSize = 15 }))
+		{
+			newHP = newHP + 0.1f;
+			if (newHP > 1) newHP = 1f;
+		}
+		if (GUI.Button(new Rect(30, 120, 100, 50), "Sub", new GUIStyle("button") { fontSize = 15 }))
+		{
+			newHP = newHP - 0.1f;
+			if (newHP < 0) newHP = 0f;
+		}
+		HP = Mathf.Lerp(HP, newHP, 0.01f);
+		GUI.HorizontalScrollbar(new Rect(30, 30, 200, 20), 0.0f,HP, 0.0f, 1.0f);
+	}
+}
---
+++ HW8/HP/Assets/Scripts/UGUI_HP.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UGUI_HP : MonoBehaviour
+{
+	private float HP = 0.0f;
+	private float newHP = 0.0f;
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.rotation = Camera.main.transform.rotation; //保持面对摄像头
+		GetComponent<Slider>().value = HP * 100;
+	}
+
+	void OnGUI()
+	{
+		if (GUI.Button(new Rect(900, 60, 100, 50), "Add", new GUIStyle("button") { fontSize = 15 }))
+		{
+			newHP = newHP + 0.01f;
+			if (newHP > 1) newHP = 1f;
+		}
+		if (GUI.Button(new Rect(900, 120, 100, 50), "Sub", new GUIStyle("button") { fontSize = 15 }))
+		{
+			newHP = newHP - 0.01f;
+			if (newHP < 0) newHP = 0f;
+		}
+		HP = Mathf.Lerp(HP, newHP, 0.01f);
+	}
+}
